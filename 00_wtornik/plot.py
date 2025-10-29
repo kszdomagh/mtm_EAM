b27a67b32a67c6035711d6fce4e0b0a921e805f2
kszdomagh <kszdom@student.agh.edu.pl>
2025-10-29 12:34:01 +0100

    oscilo csv python plot scroipt added

--- 00_wtornik/plot.py
+++ 00_wtornik/plot.py
@@ -1,12 +1,35 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Oct 27 14:40:59 2025
 
 @author: kszdom
 
 
-ELEKTRONICZNA APARATURA MEDYCZNA - 
+ELEKTRONICZNA APARATURA MEDYCZNA - WYKRESY
 """
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("oscilo_csv/sim2.csv", skiprows=2, header=None)
+
+data = df.values.flatten()
+
+# Split into pairs
+time = data[0::2]
+voltage = data[1::2]
+
+
+# Plot
+
+
+plt.plot(time, voltage, marker='o')
+plt.title("Voltage vs Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.grid(True)
+plt.show()
 
 
 
